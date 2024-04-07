cd7d8be308efd5aaded40c2540d5c4bcf71664b9
DYH <1742647821@qq.com>
2024-04-07 23:36:42 +0800

    WPF项目初始化

---
+++ IFoxCAD.WPF/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System;
+global using System.Reflection;
+global using System.Reflection.Emit;
+global using System.Windows;
+global using System.Windows.Input;
+global using System.Windows.Markup;
+global using System.Diagnostics;
