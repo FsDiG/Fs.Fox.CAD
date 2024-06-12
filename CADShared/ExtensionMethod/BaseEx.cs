028aabf58232b3999e8d33000084a11e5f1bfb6a
DYH <1742647821@qq.com>
2024-06-12 09:27:38 +0800

    添加一些基础类型的扩展

---
+++ CADShared/ExtensionMethod/BaseEx.cs
@@ -0,0 +1,17 @@
+﻿namespace IFoxCAD.Cad;
+
+/// <summary>
+/// 基础扩展
+/// </summary>
+public static class BaseEx
+{
+    /// <summary>
+    /// 判断图层名是否合法
+    /// </summary>
+    /// <param name="layerName">图层名</param>
+    /// <returns>是则返回<c>true</c></returns>
+    public static bool IsLegalLayerName(this string layerName)
+    {
+        return !string.IsNullOrWhiteSpace(layerName) && SymbolUtilityServices.RepairSymbolName(layerName, true) == layerName;
+    }
+}
