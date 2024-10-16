0423562fb5a7c22875c25d4958cff6a24a3aa015
DYH <1742647821@qq.com>
2024-10-16 21:04:50 +0800

    补充更多的系统变量支持

--- CADShared/Runtime/SystemVariableManager.cs
+++ CADShared/Runtime/SystemVariableManager.cs
@@ -1,4 +1,4 @@
-﻿using Autodesk.AutoCAD.Internal.DatabaseServices;
+﻿// ReSharper disable InconsistentNaming
 
 namespace IFoxCAD.Cad;
 
@@ -47,7 +47,95 @@ public class SystemVariableManager
 
     #endregion
 
+    #region B
+
+    /// <summary>
+    /// 是否在块编辑器中
+    /// </summary>
+    public bool BlockEditor => Acap.GetSystemVariable(nameof(BlockEditor)) is 1;
+
+    #endregion
+
     #region C
+
+    /// <summary>
+    /// 用于设置当前空间的当前注释比例的值
+    /// </summary>
+    public static string CanNoScale => Acap.GetSystemVariable(nameof(CanNoScale)).ToString();
+
+    /// <summary>
+    /// 用于显示当前的注释性比例
+    /// </summary>
+    public static double CanNoScaleValue => Convert.ToDouble(Acap.GetSystemVariable(nameof(CanNoScale)));
+
+    /// <summary>
+    /// 储存以公元纪年为基准的日历数据和时间
+    /// </summary>
+    public static double CDate => Convert.ToDouble(Acap.GetSystemVariable(nameof(CDate)));
+
+    /// <summary>
+    /// 设置新对象的颜色
+    /// </summary>
+    public static string CEColor
+    {
+        get => Acap.GetSystemVariable(nameof(CEColor)).ToString();
+        set => Acap.SetSystemVariable(nameof(CEColor), value);
+    }
+
+    /// <summary>
+    /// 设置新对象的线型比例因子
+    /// </summary>
+    public static double CELtScale
+    {
+        get => Convert.ToDouble(Acap.GetSystemVariable(nameof(CELtScale)));
+        set => Acap.SetSystemVariable(nameof(CELtScale), value);
+    }
+
+    /// <summary>
+    /// 设置新对象的线型
+    /// </summary>
+    public static string CELType
+    {
+        get => Acap.GetSystemVariable(nameof(CELType)).ToString();
+        set => Acap.SetSystemVariable(nameof(CELType), value);
+    }
+
+    /// <summary>
+    /// 设置新对象的线宽
+    /// </summary>
+    public static double CELWeight
+    {
+        get => Convert.ToDouble(Acap.GetSystemVariable(nameof(CELWeight)));
+        set => Acap.SetSystemVariable(nameof(CELWeight), value);
+    }
+
+    /// <summary>
+    /// 设置圆的默认半径
+    /// </summary>
+    public static double CircleRad
+    {
+        get => Convert.ToDouble(Acap.GetSystemVariable(nameof(CircleRad)));
+        set => Acap.SetSystemVariable(nameof(CircleRad), value);
+    }
+
+    /// <summary>
+    /// 设置当前图层
+    /// </summary>
+    public static string CLayer
+    {
+        get => Acap.GetSystemVariable(nameof(CLayer)).ToString();
+        set => Acap.SetSystemVariable(nameof(CLayer), value);
+    }
+
+    /// <summary>
+    /// 用于确定全屏显示是打开或关闭状态
+    /// </summary>
+    public static bool CleanScreenState => Convert.ToBoolean(Acap.GetSystemVariable(nameof(CleanScreenState)));
+
+    /// <summary>
+    /// 指示命令窗口是隐藏还是显示状态
+    /// </summary>
+    public static bool CliState => Convert.ToBoolean(Acap.GetSystemVariable(nameof(CliState)));
 
     /// <summary>
     /// 存在活动命令
@@ -55,11 +143,46 @@ public class SystemVariableManager
     public static bool CmdActive => Convert.ToBoolean(Acap.GetSystemVariable(nameof(CmdActive)));
 
     /// <summary>
+    /// 控制是否要打开对话框来显示命令
+    /// </summary>
+    public static bool CmdDia
+    {
+        get => Convert.ToBoolean(Acap.GetSystemVariable(nameof(CmdDia)));
+        set => Acap.SetSystemVariable(nameof(CmdDia), Convert.ToInt32(value));
+    }
+
+    /// <summary>
+    /// 在使用 LISP 的函数时，切换回应为打开或关闭
+    /// </summary>
+    public static bool CmdEcho => Convert.ToBoolean(Acap.GetSystemVariable(nameof(CmdEcho)));
+
+    /// <summary>
     /// 当前的命令
     /// </summary>
     public static string CmdNames => Acap.GetSystemVariable(nameof(CmdNames)).ToString();
 
+    /// <summary>
+    /// 返回图形中的当前选项卡（模型或布局）的名称
+    /// </summary>
+    public static string CTab
+    {
+        get => Acap.GetSystemVariable(nameof(CTab)).ToString();
+        set => Acap.SetSystemVariable(nameof(CTab), value);
+    }
 
+    /// <summary>
+    /// 指定十字光标的显示大小。输入的数字代表十字光标相对于屏幕的比例。<br/>
+    /// 范围[1, 100]
+    /// </summary>
+    public static int CursorSize
+    {
+        get => Convert.ToInt32(Acap.GetSystemVariable(nameof(CursorSize)));
+        set => Acap.SetSystemVariable(nameof(CursorSize), value);
+    }
+
+    /// <summary>
+    /// 当前viewport的编号
+    /// </summary>
     public static short CVPort
     {
         get => Convert.ToInt16(Acap.GetSystemVariable(nameof(CVPort)));
@@ -79,12 +202,23 @@ public class SystemVariableManager
         set => Acap.SetSystemVariable(nameof(DblClick), Convert.ToInt32(value));
     }
 
+    /// <summary>
+    /// 指示图形的修改状态
+    /// </summary>
+    public static DBmod DBMod => (DBmod)Acap.GetSystemVariable(nameof(DBMod));
+
+    /// <summary>
+    /// 动态输入
+    /// </summary>
     public static int DynMode
     {
         get => Convert.ToInt32(Acap.GetSystemVariable(nameof(DynMode)));
         set => Acap.SetSystemVariable(nameof(DynMode), value);
     }
 
+    /// <summary>
+    /// 动态提示
+    /// </summary>
     public static bool DynPrompt
     {
         get => Convert.ToBoolean(Acap.GetSystemVariable(nameof(DynPrompt)));
@@ -177,6 +311,19 @@ public class SystemVariableManager
     {
         get => (MeasurementValue)Acap.GetSystemVariable(nameof(Measurement));
         set => Acap.SetSystemVariable(nameof(Measurement), Convert.ToInt32(value));
+    }
+
+    #endregion
+
+    #region O
+
+    /// <summary>
+    /// 正交
+    /// </summary>
+    public static bool OrthoMode
+    {
+        get => Convert.ToBoolean(Acap.GetSystemVariable(nameof(OrthoMode)));
+        set => Acap.SetSystemVariable(nameof(OrthoMode), Convert.ToInt32(value));
     }
 
     #endregion
