d24faebfb29020425b65b1ac56aaa1683441f75f
DYH <1742647821@qq.com>
2024-11-27 16:39:44 +0800

    添加一种更新图层的方法

--- CADShared/ExtensionMethod/IFoxUtils.cs
+++ CADShared/ExtensionMethod/IFoxUtils.cs
@@ -10,8 +10,8 @@ public static class IFoxUtils
     /// <summary>
     /// 刷新图层状态，在修改图层的锁定或冻结状态后使用
     /// </summary>
-    /// <param name="objectIds">图层id集合</param>
-    public static void RegenLayers(IEnumerable<ObjectId> objectIds)
+    /// <param name="layerIds">图层id集合</param>
+    public static void RegenLayers(IEnumerable<ObjectId> layerIds)
     {
         var type = Acaop.Version.Major >= 21
             ? Assembly.Load("accoremgd")?.GetType("Autodesk.AutoCAD.Internal.CoreLayerUtilities")
@@ -19,7 +19,47 @@ public static class IFoxUtils
         var mi = type?.GetMethods().FirstOrDefault(e => e.Name == "RegenLayers");
         var pi = type?.GetProperties().FirstOrDefault(e => e.Name == "RegenPending");
         var regenPending = (int)(pi?.GetValue(null) ?? 0);
-        mi?.Invoke(null, [objectIds.ToArray(), regenPending]);
+        mi?.Invoke(null, [layerIds.ToArray(), regenPending]);
+    }
+
+    /// <summary>
+    /// 刷新图层状态，在修改图层的锁定或冻结状态后使用
+    /// </summary>
+    /// <param name="layerIds">图层id集合</param>
+    public static void RegenLayers2(IEnumerable<ObjectId> layerIds)
+    {
+        var isOff = false;
+        var layerRxc = RXObject.GetClass(typeof(LayerTableRecord));
+        var entityRxc = RXObject.GetClass(typeof(Entity));
+        foreach (var group in layerIds.Where(e => e.IsOk() && e.ObjectClass.IsDerivedFrom(layerRxc))
+                     .GroupBy(e => e.Database))
+        {
+            var db = group.Key;
+            var doc = Acap.DocumentManager.GetDocument(db);
+            if (doc is null)
+                continue; // 获取不到文档说明是后台开图，后台开图就不用刷新了
+            using var dl = doc.LockDocument();
+            using var tr = new DBTrans(db);
+            var layerIdSet = group.ToHashSet();
+            layerIdSet.Count.Print();
+            foreach (var ltr in layerIdSet.Select(id => tr.GetObject(id, OpenMode.ForWrite))
+                         .OfType<LayerTableRecord>())
+            {
+                Volatile.Write(ref isOff, ltr.IsOff);
+                ltr.IsOff = Volatile.Read(ref isOff);
+            }
+
+            for (var i = db.BlockTableId.Handle.Value; i < db.Handseed.Value; i++)
+            {
+                if (!db.TryGetObjectId(new Handle(i), out var id) || !id.IsOk() ||
+                    !id.ObjectClass.IsDerivedFrom(entityRxc))
+                    continue;
+                var ent = (Entity)tr.GetObject(id, OpenMode.ForWrite, false, true);
+                if (!layerIdSet.Contains(ent.LayerId))
+                    continue;
+                ent.RecordGraphicsModified(true);
+            }
+        }
     }
 
     /// <summary>
@@ -32,7 +72,9 @@ public static class IFoxUtils
     /// <param name="hyperText">链接</param>
     /// <param name="hyperLink">链接地址</param>
     /// <param name="text2">内容2</param>
-    public static void ShowBubbleWindow(int second, string title, string text, IconType iconType = IconType.None, string? hyperText = null, string? hyperLink = null, string? text2 = null)
+    public static void ShowBubbleWindow(int second, string title, string text,
+        IconType iconType = IconType.None, string? hyperText = null, string? hyperLink = null,
+        string? text2 = null)
     {
         TrayItem? trayItem = null;
         const string name = "IFox";
@@ -40,25 +82,25 @@ public static class IFoxUtils
         for (var i = 0; i < num; i++)
         {
             var ti = Acap.StatusBar.TrayItems[i];
-            if (ti.ToolTipText != name) continue;
+            if (ti.ToolTipText != name)
+                continue;
             trayItem = ti;
             break;
         }
 
         if (trayItem == null)
         {
-            trayItem = new()
-            {
-                ToolTipText = name,
-                Visible = true,
-            };
+            trayItem = new() { ToolTipText = name, Visible = true, };
             Acap.StatusBar.TrayItems.Add(trayItem);
             Acap.StatusBar.Update();
         }
 
-        if (second <= 0) second = 0;
-        else if (second % 10 == 0) second = 10;
-        else second %= 10;
+        if (second <= 0)
+            second = 0;
+        else if (second % 10 == 0)
+            second = 10;
+        else
+            second %= 10;
         Acaop.SetSystemVariable("TrayTimeOut", second);
         var tibw = new TrayItemBubbleWindow
         {
