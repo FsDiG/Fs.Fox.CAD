908f58f5a98acbe7a29487fb7a7dd4a978f3fd86
Bob <iChengbo@qq.com>
2026-08-01 18:17:46 +0800

    refactor: split geometry helper types

--- src/CADShared/Cad/Geometry/SpatialIndex/QuadTree/QuadTreeSelectMode.cs
+++ src/CADShared/Cad/Geometry/SpatialIndex/QuadTree/QuadTreeSelectMode.cs
@@ -14,26 +14,3 @@ public enum QuadTreeSelectMode
     /// </summary>
     Contains,     
 }
-
-/// <summary>
-/// 四叉树查找方向
-/// </summary>
-public enum QuadTreeFindMode
-{
-    /// <summary>
-    /// 上
-    /// </summary>
-    Top    = 1,  
-    /// <summary>
-    /// 下
-    /// </summary>
-    Bottom = 2,  
-    /// <summary>
-    /// 左
-    /// </summary>
-    Left   = 4, 
-    /// <summary>
-    /// 右
-    /// </summary>
-    Right  = 8,  
-}
